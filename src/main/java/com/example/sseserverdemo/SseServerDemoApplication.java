91cbef09a3ed39f36d214d316c31a228befb6bb3
MABOZ <maboz@orsted.dk>
2020-10-12 17:19:58 +0200

    add new files

---
+++ src/main/java/com/example/sseserverdemo/SseServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sseserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SseServerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sseserverdemo/SseServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sseserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseServerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
