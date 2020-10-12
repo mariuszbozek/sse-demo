f0f4cb4a973297d05f27a7c8dfd7d0d2185ada76
mariuszbozek <44567602+mariuszbozek@users.noreply.github.com>
2020-10-12 17:35:18 +0200

    Update SseServerDemoApplication.java

--- src/main/java/com/example/sseserverdemo/SseServerDemoApplication.java
+++ src/main/java/com/example/sseserverdemo/SseServerDemoApplication.java
@@ -2,12 +2,23 @@ package com.example.sseserverdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 public class SseServerDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SseServerDemoApplication.class, args);
+    }
+    
+    @Bean
+    public SecurityWebFilterChain sseServerSpringSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange()
+                .anyExchange()
+                .permitAll();
+        return http.build();
     }
 
 }
